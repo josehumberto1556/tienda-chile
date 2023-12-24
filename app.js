d7c29db576748d35f79a67f47dcdc06e344a361e
josehumberto1556 <josehumberto1556@gmail.com>
2023-12-23 22:51:31 -0400

    24-12-2024 segundo commit

--- app.js
+++ app.js
@@ -5,18 +5,41 @@ const cors = require('cors');
 //dotenv.config();
 const app = express();
 
+//motor de plantllas
+app.set('view engine','ejs')
+app.set('views',__dirname+'/views')
+
 //const routerApi = require('./routes');
 
 const port = process.env.PORT || 3000;
-
+app.use(express.static(__dirname+"/public/"))
 app.use(cors());
 app.use(express.json());
 
+
+
 app.get('/', (req,res) => {
-    res.send('Backend con NodeJS - Express + CRUD API REST + MySQL');
+   // res.send('Backend con NodeJS - Express + CRUD API REST + MySQL');
+   res.render("index",{titulo:"titulo dinamico",sorangel:"es bonita"})
+});
+
+
+app.get('/', (req,res) => {
+   // res.send('Backend con NodeJS - Express + CRUD API REST + MySQL');
+  // res.render("index",{titulo:"titulo dinamico"})
+   res.send("me gusta sorangel")
 });
 
 //routerApi(app);
+
+
+
+app.use((req,res,next) => {
+   // res.send('Backend con NodeJS - Express + CRUD API REST + MySQL');
+   //res.status(404).sendFile(__dirname+"/public/404.html")
+   res.status(404).render("404")
+});
+
 
 app.listen(port,()=>{
     console.log("Port ==> ", port);
